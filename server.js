6d4649ddf10e2b453ebbd2efb998ea8b8d3f0524
Wilfredo Alcala <alcalawilfre@gmail.com>
2020-05-03 18:50:35 -0300

    use paypal checkout sdk

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const paypal = require("paypal-rest-sdk");
+const paypal = require("@paypal/checkout-server-sdk");
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -10,67 +10,72 @@ const SERVER_PORT = Number(process.env.SERVER_PORT || 5000);
 const CLIENT_ID = process.env.CLIENT_ID;
 const SECRET = process.env.SECRET;
 
-// configure paypal with the credentials you got when you created your paypal app
-paypal.configure({
-  mode: "sandbox", //sandbox or live
-  client_id: CLIENT_ID, // please provide your client id here
-  client_secret: SECRET, // provide your client secret here
-});
+let environment = new paypal.core.SandboxEnvironment(CLIENT_ID, SECRET);
+let client = new paypal.core.PayPalHttpClient(environment);
 
 // set public directory to serve static html files
 app.use("/", express.static(path.join(__dirname, "public")));
-
+app.use(express.json());
 // redirect to store when user hits http://localhost:3000
 app.get("/", (req, res) => {
   res.redirect("/index.html");
 });
 
 // start payment process
-app.get("/buy", (req, res) => {
-  // create payment object
-  const payment = {
-    intent: "authorize",
-    payer: {
-      payment_method: "paypal",
-    },
-    redirect_urls: {
-      return_url: SUCCESS_URL,
-      cancel_url: ERROR_URL,
-    },
-    transactions: [
+app.post("/buy", async (req, res) => {
+  console.log("Buy called");
+  const request = new paypal.orders.OrdersCreateRequest();
+  request.requestBody({
+    intent: "CAPTURE",
+    purchase_units: [
       {
         amount: {
-          total: 39.0,
-          currency: "USD",
+          currency_code: "USD",
+          value: "5.00",
         },
-        description: " a book on mean stack ",
       },
     ],
-  };
+  });
 
-  // call the create Pay method
-  createPay(payment)
-    .then((transaction) => {
-      var id = transaction.id;
-      var links = transaction.links;
-      var counter = links.length;
-      while (counter--) {
-        if (links[counter].method == "REDIRECT") {
-          // redirect to paypal where user approves the transaction
-          return res.redirect(links[counter].href);
-        }
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.redirect("/error.html");
-    });
+  let order;
+  try {
+    order = await client.execute(request);
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
+  console.log("Order created");
+  console.log(order);
+  res.status(200).json({
+    orderID: order.result.id,
+  });
 });
 
 // success page
-app.get("/success", (req, res) => {
-  console.log(req.query);
-  res.redirect("/success.html");
+app.post("/capture", async (req, res) => {
+  console.log(req.body);
+  // 2a. Get the order ID from the request body
+  const orderID = req.body.orderID;
+
+  // 3. Call PayPal to capture the order
+  const request = new paypal.orders.OrdersCaptureRequest(orderID);
+  request.requestBody({});
+
+  try {
+    const capture = await client.execute(request);
+
+    // 4. Save the capture ID to your database. Implement logic to save capture to your database for future reference.
+    const captureID = capture.result.purchase_units[0].payments.captures[0].id;
+    // await database.saveCaptureID(captureID);
+    console.log("Details", capture.result);
+    console.log("CAPTUREDDDDD", captureID);
+    res.json({
+      payerName: capture.result.payer.name.given_name,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.sendStatus(500);
+  }
 });
 
 // error page
